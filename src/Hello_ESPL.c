36bea86d95a358976d295ea0b4f2e8fa569c4738
Ala <afnayou@gmail.com>
2020-05-10 18:35:48 +0200

    Coding challenge 2 successfully implemented

--- src/Hello_ESPL.c
+++ src/Hello_ESPL.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "espl_lib.h"
+
 int main(){
-printf("Hello ESPL\n");
-printf("This is a new print\n");
-return 0;
+
+	int number,a=1;
+	char c;
+	while(a!=0){
+		printf("Please choose a number : ");
+		scanf("%d", &number);
+	      	getchar ();
+        	printf("%s\n",num_to_words(number));
+        	printf("Type the letter x to exit or the letter r to choose another number : ");
+        	scanf("%c", &c);
+        	if (c  == 'r') { 
+              		a=1; 
+		}
+        	else { 
+              		a=0; 
+		}
+	}
 }
